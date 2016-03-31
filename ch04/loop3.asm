65fa3833b0b19f7e03c2eef1de03b5159c291c2d
noprom <noprom@nopromdeMacBook-Pro.local>
2016-03-31 15:33:16 +0800

    loop to cal 123*236

---
+++ ch04/loop3.asm
@@ -0,0 +1,17 @@
+;; Author: noprom <tyee.noprom@qq.com>
+;; Data: 2016/3/31 3:32PM
+;; Title: 利用loop计算123*236的小程序
+
+assume cs: code
+code segment
+  mov ax, 0
+  mov cx, 123
+
+s:add ax, 236
+  loop s
+
+  ;;退出程序
+  mov ax, 4c00H
+  int 21H
+code ends
+end
