04e1a70babcab02327a531bbf906b1db0e017032
noprom <tyee.noprom@qq.com>
2016-04-06 10:34:54 +0800

    add p6-6

---
+++ ch06/p6-6.asm
@@ -0,0 +1,33 @@
+;; Author: noprom <tyee.noprom@qq.com>
+;; Data: 2016/4/6 10:29AM
+;; Title: 实验五:编写调试具有多个段的程序
+
+assume cs: code, ds: data, ss: stack
+;数据段
+data segment
+  dw 0123h, 0456h, 0789h, 0abch, 0defh, 0fedh, 0cbah, 0987h
+data ends
+
+;栈段
+stack segment
+  dw 0, 0, 0, 0, 0, 0, 0, 0
+stack ends
+
+;代码段
+code segment
+  start: mov ax, stack
+         mov ss, ax
+         mov sp, 16
+
+         mov ax, data
+         mov ds, ax
+
+         push ds:[0]
+         push ds:[2]
+         pop ds:[2]
+         pop ds:[0]
+
+         mov ax, 4c00h
+         int 21h
+code ends
+end start
