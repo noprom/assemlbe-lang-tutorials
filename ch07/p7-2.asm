c20d984a56e59640b8dd2c49574087c632a58c39
noprom <tyee.noprom@qq.com>
2016-04-11 09:47:52 +0800

    init p7-2

---
+++ ch07/p7-2.asm
@@ -0,0 +1,12 @@
+;; Author: noprom <tyee.noprom@qq.com>
+;; Data: 2016/4/11 9:45AM
+;; Title: 程序7.2:将datasg中第一个字符串转化为大写,第二个字符串转化为小写
+assume cs: codesg, ds: datasg
+datasg segment
+datasg ends
+
+codesg segment
+start:
+
+codesg ends
+end start
