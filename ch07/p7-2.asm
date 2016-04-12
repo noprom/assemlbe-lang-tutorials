3c33a2db2ce07be181e9299f06d0c1efdfc5ef5f
noprom <tyee.noprom@qq.com>
2016-04-12 09:30:58 +0800

    convert successfully

--- ch07/p7-2.asm
+++ ch07/p7-2.asm
@@ -9,6 +9,24 @@ datasg ends
 
 codesg segment
 start:
+  mov ax, datasg
+  mov ds, ax
+  mov bx, 0               ;ds:bx指向'BaSiC'第一个字母
 
+  mov cx, 5
+s:mov al, [bx]
+  and al, 11011111B       ;第五位变为0,转化为大写
+  inc bx
+  loop s
+
+  mov bx, 5               ;ds:bx指向'iNfOrMaTiOn'第一个字母
+  mov cx, 11
+p:mov al, [bx]
+  or al, 00100000B        ;第五位变为1,转化为小写
+  inc bx
+  loop p
+
+  mov ax, 4c00h
+  int 21h
 codesg ends
 end start
