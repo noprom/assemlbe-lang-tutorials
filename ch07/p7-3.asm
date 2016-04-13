0ff5ce7f90046d76972eff8b9c312457bcf3f0d7
noprom <tyee.noprom@qq.com>
2016-04-13 10:41:25 +0800

    add array query

--- ch07/p7-3.asm
+++ ch07/p7-3.asm
@@ -9,6 +9,19 @@ datasg ends
 
 codesg segment
 start:
+  mov ax, datasg
+  mov ds, ax
+  mov bx, 0
+
+  mov cx, 5
+s:mov al, [bx]
+  and al, 1101111B
+  mov [bx], al
+  mov al, [5+bx]
+  or al, 0010000B
+  mov [5+bx], al
+  inc bx
+  loop s
 
   mov ax, 4c00h
   int 21h
